10d4dad230f5212362c60757a6287491f094ff5f
jansseneda-adobe <danimaejanssen@gmail.com>
2020-10-02 17:07:40 -0500

    message

--- .build/.appveyor/build.ps1
+++ .build/.appveyor/build.ps1
@@ -21,6 +21,7 @@ mkdir release
 
 if (${env:APPVEYOR_REPO_TAG} -eq $true)
 {
+    Write-Host "Archiving artifacts for tagged commit"
     cp dist\user-sync.exe release\
     cd release
     7z a "user-sync-${env:APPVEYOR_REPO_TAG_NAME}${env:BUILD_EDITION}-win64.zip" user-sync.exe
@@ -29,4 +30,6 @@ if (${env:APPVEYOR_REPO_TAG} -eq $true)
     7z a -tgzip release\examples.tar.gz release\examples.tar
     7z a release\examples.zip examples\
     dir release
+} else {
+    Write-Host "Skipping artifact archival for non-tagged commit"
 }
